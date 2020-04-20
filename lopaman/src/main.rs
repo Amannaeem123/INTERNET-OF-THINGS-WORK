f0476b9d49fa3b4d37e84530b1aeacd14bb931fe
Aman Naeem <63961704+Amannaeem123@users.noreply.github.com>
2020-04-20 07:56:13 +0500

    Add files via upload

---
+++ lopaman/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+
+let mut a = String::new();
+
+for _i in 0..8{
+
+for _r in 0..1{
+
+a.push_str("*");
+
+println!("{}" , a);
+
+}
+
+}
+
+}
+ 
+
---
+++ lopaman/target/debug/deps/lopaman-17db926480e79113.d
@@ -0,0 +1,5 @@
+/home/aman/lopaman/target/debug/deps/lopaman-17db926480e79113: src/main.rs
+
+/home/aman/lopaman/target/debug/deps/lopaman-17db926480e79113.d: src/main.rs
+
+src/main.rs:

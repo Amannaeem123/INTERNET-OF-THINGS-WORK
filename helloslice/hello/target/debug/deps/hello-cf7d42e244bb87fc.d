7af81adac7dff7cbe8d9048d65141b7bcbe00c64
Aman Naeem <63961704+Amannaeem123@users.noreply.github.com>
2020-04-20 07:45:32 +0500

    Add files via upload

---
+++ helloslice/hello/target/debug/deps/hello-cf7d42e244bb87fc.d
@@ -0,0 +1,5 @@
+/home/aman/hello/target/debug/deps/hello-cf7d42e244bb87fc: src/main.rs
+
+/home/aman/hello/target/debug/deps/hello-cf7d42e244bb87fc.d: src/main.rs
+
+src/main.rs:
---
+++ helloslice/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/aman/hello/target/debug/hello: /home/aman/hello/src/main.rs

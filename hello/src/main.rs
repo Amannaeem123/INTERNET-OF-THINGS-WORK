bc74ebfcac05ebedc9314573c64f43dc8489bf37
Aman Naeem <63961704+Amannaeem123@users.noreply.github.com>
2020-04-20 07:43:09 +0500

    Add files via upload

---
+++ hello/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+let mut number1 = 1;
+
+let mut number2 = 2;
+
+for _i in 0..5{
+
+// 1 2 3 5 8
+
+let result = number1 + number2;
+
+number1 = number2;
+number2 = result;
+
+// 1 + 2 = 3
+// 1 2 3
+
+println!("{}" ,result);
+}
+} 

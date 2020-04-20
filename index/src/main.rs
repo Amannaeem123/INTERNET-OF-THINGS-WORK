27d07f77a61f341f955847656cdbe756178d78d8
Aman Naeem <63961704+Amannaeem123@users.noreply.github.com>
2020-04-20 07:51:10 +0500

    Add files via upload

---
+++ index/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let x = [1,2,3,4,5];
+for index in x.iter() {
+//println!("the value of a is {}",index);
+index * = 2;
+}
+println!("{#?}",index);
+}
---
+++ index/target/debug/deps/index-d344af280f6cfa4a.d
@@ -0,0 +1,5 @@
+/home/aman/index/target/debug/deps/index-d344af280f6cfa4a: src/main.rs
+
+/home/aman/index/target/debug/deps/index-d344af280f6cfa4a.d: src/main.rs
+
+src/main.rs:

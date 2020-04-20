1c7c81a29e8e264c3d418769c51e600339bc902f
Aman Naeem <63961704+Amannaeem123@users.noreply.github.com>
2020-04-20 07:59:32 +0500

    Add files via upload

---
+++ musab/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+ let x = "musab";
+let mut x = "aman";
+println!("the value of x is {}",x)
+}
---
+++ musab/target/debug/deps/musab-0cc4b07c77f69025.d
@@ -0,0 +1,5 @@
+/home/aman/musab/target/debug/deps/musab-0cc4b07c77f69025: src/main.rs
+
+/home/aman/musab/target/debug/deps/musab-0cc4b07c77f69025.d: src/main.rs
+
+src/main.rs:

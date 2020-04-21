2c82e9e6255345b127adb273fd0f804c047d043a
Aman Naeem <63961704+Amannaeem123@users.noreply.github.com>
2020-04-22 01:43:53 +0500

    Add files via upload

---
+++ string_char/src/main.rs
@@ -0,0 +1,7 @@
+
+#![allow(unused_variables)]
+fn main() {
+for c in "नमस्ते".chars() {
+    println!("{}", c);
+}
+}
---
+++ string_char/target/debug/deps/string_char-9c63cad8f9e8eb4b.d
@@ -0,0 +1,5 @@
+/home/aman/string_char/target/debug/deps/string_char-9c63cad8f9e8eb4b: src/main.rs
+
+/home/aman/string_char/target/debug/deps/string_char-9c63cad8f9e8eb4b.d: src/main.rs
+
+src/main.rs:

49807246c314b78fda56553e9aa885ba0a0b24ac
Aman Naeem <63961704+Amannaeem123@users.noreply.github.com>
2020-04-22 01:41:44 +0500

    Add files via upload

---
+++ stack/src/main.rs
@@ -0,0 +1,8 @@
+
+#![allow(unused_variables)]
+fn main() {
+let x = 5;
+let y = x;
+
+println!("x = {}, y = {}", x, y);
+}
---
+++ stack/target/debug/deps/stack-eebeffe4cf9931bd.d
@@ -0,0 +1,5 @@
+/home/aman/stack/target/debug/deps/stack-eebeffe4cf9931bd: src/main.rs
+
+/home/aman/stack/target/debug/deps/stack-eebeffe4cf9931bd.d: src/main.rs
+
+src/main.rs:

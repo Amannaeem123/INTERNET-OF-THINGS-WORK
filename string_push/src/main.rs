de5fc76c61ad8a842dc6bf4ddb194ca3b6d8c38f
Aman Naeem <63961704+Amannaeem123@users.noreply.github.com>
2020-04-22 08:49:06 +0500

    Add files via upload

---
+++ string_push/src/main.rs
@@ -0,0 +1,8 @@
+
+#![allow(unused_variables)]
+fn main() {
+let mut s1 = String::from("foo");
+let s2 = "bar";
+s1.push_str(s2);
+println!("s2 is {}", s2);
+}
---
+++ string_push/target/debug/deps/string_push-ddc9cb840d2575ef.d
@@ -0,0 +1,5 @@
+/home/aman/string_push/target/debug/deps/string_push-ddc9cb840d2575ef: src/main.rs
+
+/home/aman/string_push/target/debug/deps/string_push-ddc9cb840d2575ef.d: src/main.rs
+
+src/main.rs:

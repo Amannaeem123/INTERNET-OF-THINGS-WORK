17b876c92474634c8148bcca22d62f337a68b96f
Aman Naeem <63961704+Amannaeem123@users.noreply.github.com>
2020-04-22 08:53:01 +0500

    Add files via upload

---
+++ struck_tupples/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let rect1 = (120, 200);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(rect1)
+    );
+}
+
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
---
+++ struck_tupples/target/debug/deps/struck_tupples-de5dd35542c56fc6.d
@@ -0,0 +1,5 @@
+/home/aman/struck_tupples/target/debug/deps/struck_tupples-de5dd35542c56fc6: src/main.rs
+
+/home/aman/struck_tupples/target/debug/deps/struck_tupples-de5dd35542c56fc6.d: src/main.rs
+
+src/main.rs:
